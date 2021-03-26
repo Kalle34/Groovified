fdc8a875abfc63e797d136441c718cc4d4c6a388
DerEchteKroate <47820443+DerEchteKroate@users.noreply.github.com>
2021-03-26 10:29:00 +0100

    Add files via upload
    Added Youtube support

--- init.js
+++ init.js
@@ -6,6 +6,12 @@
       btn.id = "play-extension-btn";
       btn.type = "play-extension";
       document.querySelector('._95e9f2bdf9c64702a6123c2d6de8076b-scss').appendChild(btn);
+    } else if(window.location.hostname.toString() == "www.youtube.com"){
+        var ytbtn = document.createElement("button");
+        ytbtn.textContent = "Play";
+        ytbtn.id = "play-extension-ytbtn";
+        ytbtn.type = "play-extension";
+        document.querySelector('.title').appendChild(ytbtn);
     }
   }
 
@@ -16,11 +22,17 @@
             chrome.runtime.sendMessage("start");
             chrome.runtime.sendMessage(link);
         });
+      } else if(window.location.hostname.toString() == "www.youtube.com"){
+        document.getElementById("play-extension-ytbtn").addEventListener("click", function (event) {
+            link = window.location.href
+            chrome.runtime.sendMessage("start");
+            chrome.runtime.sendMessage(link);
+        });
       }
   }
 
     setTimeout(function(){
         fnAddButtons();
         fnDefineEvents();
-    }, 2000);
+    }, 1500);
 })();
