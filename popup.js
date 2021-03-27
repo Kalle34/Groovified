3ae4d136a7a3960072da203a2f246d05494dd679
DerEchteKroate <47820443+DerEchteKroate@users.noreply.github.com>
2021-03-27 20:52:03 +0100

    Add files via upload

--- popup.js
+++ popup.js
@@ -1,4 +1,5 @@
-chrome.runtime.getBackgroundPage(function(bg){
+window.onload = function() {
+    chrome.runtime.getBackgroundPage(function(bg){
     if(bg.sessionDataHTML){
          document.body.innerHTML = bg.sessionDataHTML;
     }
@@ -7,8 +8,9 @@ chrome.runtime.getBackgroundPage(function(bg){
         bg.sessionDataHTML = document.body.innerHTML
     },1000);
 
-    document.getElementById('button').onclick = function() {
+    document.getElementById('setupbutton').onclick = function() {
         link = "popup." + document.getElementById("link").value
         chrome.runtime.sendMessage(link)
     }
-});
+    });
+};
